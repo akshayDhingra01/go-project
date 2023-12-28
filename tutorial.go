eb5f8d08853c8c65e2fc7ed815289295bb6b9784
akshayDhingra01 <a4akshayd@gmail.com>
2023-12-28 20:37:56 +0530

    ADD: User name and age to the quic

---
+++ tutorial.go
@@ -0,0 +1,10 @@
+package main
+
+import "fmt"  // collect user input 
+
+func main() {
+	fmt.Println("Welcome to the quiz")
+	name := "Akshay"
+	age := 23
+	fmt.Printf("Hello %v, you are %v years old?" , name, age)
+ }
